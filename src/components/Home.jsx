1e2500a713ff287e3f0e6ca53b03d09c7118938e
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-06 12:25:03 -0400

    add Routes

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+    
+class Home extends Component {
+    render() {
+        return (
+            <div>
+              <img src="https://bsmedia.business-standard.com/_media/bs/img/article/2015-08/28/full/1440706704-1419.jpg" alt="bank"/>
+              <h1>Bank of React</h1>
+            </div>
+        );
+    }
+}
+    
+export default Home;
