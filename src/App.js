9f63f89b7a199d2e49ce2dab8d98ba8669515d73
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-06 18:06:36 -0400

    adding Debits Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import "./App.css"
 import Home from './components/Home'
 import UserProfile from './components/UserProfile'
 import LogIn from './LogIn'
+import Debits from './components/Debits'
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      accountBalance: 14568.27,
+      accountBalance:{
+        debits:"",
+        credits:"",
+      },
       currentUser:{
         userName:'bob-loblaw',
         memberSince: '08/23/99',
@@ -17,21 +22,24 @@ class App extends Component {
   }
 
   mockLogIn=(logInInfo)=>{
-    const newUser={...this.state.currentUser} //appending currentUser 
+    const newUser={...this.state.currentUser} //inherate attributes of this.state.currentUser
     newUser.userName = logInInfo.userName
     this.setState({currentUser: newUser})
   }
   
   render() {
-    const HomeComponent=()=>(<Home accountBalance={this.state.accountBalance}/>);
+    const HomeComponent=()=>(<Home debits={this.state.accountBalance.debits} credit={this.state.accountBalance.credits}/>);
+    // const HomeComponent=()=>(<Home accountBalance={this.state.accountBalance}/>);
     const UserProfileComponent=()=>(<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>);
     const LogInComponent=()=>(<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn}{...this.props}/>)
+    const DebitsComponent=()=>(<Debits debits={this.state.accountBalance.debits}/>)
     return (
       <Router>
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/login" render={LogInComponent}/>
+          <Route exact path="/Debits" render={DebitsComponent}/>
         </div>    
       </Router>
     );
