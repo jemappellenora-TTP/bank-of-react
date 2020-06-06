e74a3d3ea26b8f15355933e15795823670acf85a
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-06 12:36:54 -0400

    Passing HomeComponent

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Home from './components/Home'
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      accountBalance: 14568.27
+    }
+  }
   render() {
+    const HomeComponent=()=>(<Home accountBalance={this.state.accountBalance}/>)
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={HomeComponent}/>
         </Switch>    
       </Router>
     );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AccountBalance from './AccountBalance'
     
 class Home extends Component {
     render() {
@@ -6,6 +7,8 @@ class Home extends Component {
             <div>
               <img src="https://bsmedia.business-standard.com/_media/bs/img/article/2015-08/28/full/1440706704-1419.jpg" alt="bank"/>
               <h1>Bank of React</h1>
+
+              <AccountBalance accountBalance={this.props.accountBalance}/>
             </div>
         );
     }
