116a06536806c438c6aef5b6733b2a099ebac658
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-06 17:10:02 -0400

    navigate to Debits

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,13 @@ class Home extends Component {
         <div>
           <img src="https://bsmedia.business-standard.com/_media/bs/img/article/2015-08/28/full/1440706704-1419.jpg" alt="bank"/>
           <h1>Bank of React</h1>
-
+          <Link to="/Debits">Debits</Link>
+          <br/>
           <Link to="/userProfile">User Profile</Link>
           <br/>
           <Link to="/LogIn">Login to Your Account</Link>
 
-          <AccountBalance accountBalance={this.props.accountBalance}/>
+          <AccountBalance debits={this.props.debits} credits={this.props.credits}/>
         </div>
     );
   }
