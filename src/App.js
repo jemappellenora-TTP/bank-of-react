c404690adb8fc0a0e6821d0fc17150d3051bb0b6
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-06 13:09:17 -0400

    Routing to UserProfile Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Home from './components/Home'
+import UserProfile from './components/UserProfile'
+
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      accountBalance: 14568.27
+      accountBalance: 14568.27,
+      currentUser:{
+        userName:'bob-loblaw',
+        memberSince: '08/23/99',
+      }
     }
   }
   render() {
-    const HomeComponent=()=>(<Home accountBalance={this.state.accountBalance}/>)
+    const HomeComponent=()=>(<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent=()=>(<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>);
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={HomeComponent}/>
-        </Switch>    
+        <div>
+          <Route exact path="/" render={HomeComponent}/>
+          <Route exact path="/userProfile" render={UserProfileComponent}/>
+        </div>    
       </Router>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
-import AccountBalance from './AccountBalance'
-    
-class Home extends Component {
-    render() {
-        return (
-            <div>
-              <img src="https://bsmedia.business-standard.com/_media/bs/img/article/2015-08/28/full/1440706704-1419.jpg" alt="bank"/>
-              <h1>Bank of React</h1>
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
 
-              <AccountBalance accountBalance={this.props.accountBalance}/>
-            </div>
-        );
-    }
+class Home extends Component {
+  render() {
+    return (
+        <div>
+          <img src="https://bsmedia.business-standard.com/_media/bs/img/article/2015-08/28/full/1440706704-1419.jpg" alt="bank"/>
+          <h1>Bank of React</h1>
+
+          <Link to="/userProfile">User Profile</Link>
+
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+        </div>
+    );
+  }
 }
-    
+
 export default Home;
