32c47f559fba42d531ed2d7ac76b36f490d0d724
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-08 09:40:49 -0400

    Updates

---
+++ precache-manifest.2b99b921a92cbbcb6367cecc0833c6c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a9ef128462f4161b6dae00e91664b9",
+    "url": "/bank-of-react/index.html"
+  },
+  {
+    "revision": "031cfb1bd38a85d5e479",
+    "url": "/bank-of-react/static/css/main.c6f867c3.chunk.css"
+  },
+  {
+    "revision": "b55b44cd4cbb874d2b22",
+    "url": "/bank-of-react/static/js/2.7a3cde51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bank-of-react/static/js/2.7a3cde51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031cfb1bd38a85d5e479",
+    "url": "/bank-of-react/static/js/main.9480800a.chunk.js"
+  },
+  {
+    "revision": "648ea6b921ae95e7eb38",
+    "url": "/bank-of-react/static/js/runtime-main.04df8d7a.js"
+  }
+]);
